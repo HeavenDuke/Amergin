8b93f93a7e706f78547608a96ee49607ddd06200
HeavenDuke <trashlhc@163.com>
2014-12-16 16:19:56 +0800

    备份
    完成获取UPDSEED的逻辑的90%。

--- Web-Part/music/refreshcluster.php
+++ Web-Part/music/refreshcluster.php
@@ -24,24 +24,106 @@
 	$Music_Softness=$_POST['Softness'];
 	$Music_Class=0;
 	$Music_Neibour=0;
+	$SEED=array();
 
-	$SQL_FETCH1='SELECT mid FROM music
-				 WHERE neibour=?
-				 AND (speed-?)*(speed-?)+(normality-?)*(normality-?)+(stability-?)*(stability-?)+
+	$SQL_FETCH1='SELECT mid,speed,normality,stability,happiness,
+						ease,depression,craziness,enthusiastism,
+						grief,softness,class
+				 FROM music
+				 WHERE neibour=?-1
+				 AND ((speed-?)*(speed-?)+(normality-?)*(normality-?)+(stability-?)*(stability-?)+
 				 	 (happiness-?)*(happiness-?)+(ease-?)*(ease-?)+(depression-?)*(depression-?)+
 				 	 (craziness-?)*(craziness-?)+(enthusiastism-?)*(enthusiastism-?)+(grief-?)*(grief-?)+
-				 	 (softness-?)*(softness-?)<?';
+				 	 (softness-?)*(softness-?))<?';
+
+	$SQL_INSERTP='INSERT INTO music(name,artist,album,speed,stability,normality,
+									happiness,ease,depression,craziness,enthusiastism,
+									grief,softness,class,neibour) 
+				  VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,0,?)';
+
+	$SQL_FINDPID='SELECT mid FROM music 
+				  WHERE name=? AND artist=? AND album=?';
+
+	$SQL_UPDATE1='UPDATE music SET neibour=neibour+1
+				  WHERE ((speed-?)*(speed-?)+(normality-?)*(normality-?)+(stability-?)*(stability-?)+
+				 	 	(happiness-?)*(happiness-?)+(ease-?)*(ease-?)+(depression-?)*(depression-?)+
+				 	 	(craziness-?)*(craziness-?)+(enthusiastism-?)*(enthusiastism-?)+(grief-?)*(grief-?)+
+				 	 	(softness-?)*(softness-?))<?';
 
 	$mysqli=new mysqli($sql_server,$sql_username,$sql_password,$sql_database,$sql_port);
 	$mysqli_query=$mysqli->prepare($SQL_FETCH1);
-	$mysqli_query->bind_param('ii',$MINPTS,$EPS);
+	$mysqli_query->bind_param('iiiiiiiiiiii',$MINPTS,$Music_Speed,$Music_Stability,
+								   			 $Music_Normality,$Music_Happiness,$Music_Ease,
+								   			 $Music_Depression,$Music_Craziness,$Music_Enthusiastism,
+								   			 $Music_Grief,$Music_Softness,$EPS);
 	$mysqli_query->execute();
-	$mysqli_query->bind_result($music);
+	$mysqli_query->bind_result($mid,$speed,$stability,$normality,
+								$happiness,$ease,$depression,$craziness,
+								$enthusiastism,$grief,$softness,$class);
 	$q0=array();
 	while($mysqli_query->fetch()){
-		array_push($q0, $music);
+		$temp['id']=$mid;
+		$temp['speed']=$speed;
+		$temp['stability']=$stability;
+		$temp['normality']=$normality;
+		$temp['happiness']=$happiness;
+		$temp['ease']=$ease;
+		$temp['depression']=$depression;
+		$temp['craziness']=$craziness;
+		$temp['enthusiastism']=$enthusiastism;
+		$temp['grief']=$grief;
+		$temp['softness']=$softness;
+		$temp['class']=$class;
+		array_push($q0, $temp);
 	}
+	$mysqli->close();
+
 	$Music_Neibour=count($q0);
+	
+	$mysqli=new mysqli($sql_server,$sql_username,$sql_password,$sql_database,$sql_port);
+	$mysqli_query=$mysqli->prepare($SQL_FETCH1);
+	$mysqli_query->bind_param('sssiiiiiiiiiii',$Music_Name,$Music_Artist,$Music_Album,
+											   $Music_Speed,$Music_Stability,$Music_Normality,
+											   $Music_Happiness,$Music_Ease,$Music_Depression,
+											   $Music_Craziness,$Music_Enthusiastism,$Music_Grief,
+											   $Music_Softness,$Music_Neibour);
+	$mysqli_query->execute();
+	$mysqli->close();
+
+	$mysqli=new mysqli($sql_server,$sql_username,$sql_password,$sql_database,$sql_port);
+	$mysqli_query=$mysqli->prepare($SQL_FINDPID);
+	$mysqli_query->bind_param('sss',$Music_Name,$Music_Artist,$Music_Album);
+	$mysqli_query->execute();
+	$mysqli_query->bind_result($MUSIC_PID);
+	$mysqli_QUERY->fetch();
+	$mysqli->close();
+
+	$mysqli=new mysqli($sql_server,$sql_username,$sql_password,$sql_database,$sql_port);
+	$mysqli_query=$mysqli->prepare($SQL_UPDATE1);
+	$mysqli_query->bind_param('iiiiiiiiii',$Music_Speed,$Music_Stability,
+								   		   $Music_Normality,$Music_Happiness,$Music_Ease,
+								   		   $Music_Depression,$Music_Craziness,$Music_Enthusiastism,
+								   		   $Music_Grief,$Music_Softness);
+	$mysqli_query->execute();
+	$mysqli->close();
+
+	$SQL_FETCHQS='SELECT mid,class FROM music
+				  WHERE neibour>=?
+				  AND ((speed-?)*(speed-?)+(normality-?)*(normality-?)+(stability-?)*(stability-?)+
+				 	  (happiness-?)*(happiness-?)+(ease-?)*(ease-?)+(depression-?)*(depression-?)+
+				 	  (craziness-?)*(craziness-?)+(enthusiastism-?)*(enthusiastism-?)+(grief-?)*(grief-?)+
+				 	  (softness-?)*(softness-?))<?';
+
+	for($i=0;$i<$Music_Neibour;$i++){
+		$MUSIC_QID=$q0[$i];
+		$mysqli=new mysqli($sql_server,$sql_username,$sql_password,$sql_database,$sql_port);
+		$mysqli_query=$mysqli->prepare($SQL_FINDPID);
+		$mysqli_query->bind_param('iiiiiiiiiiiiiiiiiiiiiiii',);
+		$mysqli_query->execute();
+		$mysqli_query->bind_result($MUSIC_PID);
+		$mysqli_QUERY->fetch();
+		$mysqli->close();
+	}
 
     function UpdateMusic($pervious,$new){
 
